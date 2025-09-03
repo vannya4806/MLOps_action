6108941f9f3fee518be2bd4dd5e565a8c3a0e5ad
Vannya Ade Gunawan <vannyaade@student.ub.ac.id>
2025-09-03 23:16:13 +0700

    Create model.py

---
+++ src/model.py
@@ -0,0 +1,17 @@
+from sklearn.linear_model import LogisticRegression
+
+class MLModel:
+    def __init__(self):
+        self.model = LogisticRegression()
+
+    def train(self, X, y):
+        self.model.fit(X, y)
+
+    def evaluate(self, X, y):
+        return self.model.score(X, y)
+
+    def predict(self, X):
+        return self.model.predict(X)
+
+def create_model():
+    return MLModel()
